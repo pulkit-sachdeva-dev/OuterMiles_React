b41931887010b2eff60b2cd12fa72654970fd6db
Sakshi Saini <Sainilalita925@gmail.com>
2025-11-17 18:26:52 +0530

    Add Signup route and clean up quotes in App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,30 +1,35 @@
-import React from 'react';
-import { Routes, Route, Navigate } from 'react-router-dom';
-import Home from './pages/Home';
-import Dashboard from './pages/Dashboard';
+import React from "react";
+import { Routes, Route, Navigate } from "react-router-dom";
+import Home from "./pages/Home";
+import Dashboard from "./pages/Dashboard";
+import Signup from "./pages/Signup";
 
 // Protected Route component to check authentication
 const ProtectedRoute = ({ children }) => {
-  const loggedInUser = JSON.parse(localStorage.getItem('loggedInUser'));
-  
+  const loggedInUser = JSON.parse(localStorage.getItem("loggedInUser"));
+
   if (!loggedInUser) {
     return <Navigate to="/" replace />;
   }
-  
+
   return children;
 };
 
 export default function App() {
   return (
     <Routes>
+      {/* Public Routes */}
       <Route path="/" element={<Home />} />
-      <Route 
-        path="/dashboard" 
+      <Route path="/signup" element={<Signup />} />
+
+      {/* Protected Route */}
+      <Route
+        path="/dashboard"
         element={
           <ProtectedRoute>
             <Dashboard />
           </ProtectedRoute>
-        } 
+        }
       />
     </Routes>
   );
